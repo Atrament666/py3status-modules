452d8ec8c128217bf008e65166a66635d1be0d5b
Atrament <atrament666@gmail.com>
2019-05-13 10:52:41 +0100

    Update myclementine.py

--- myclementine.py
+++ myclementine.py
@@ -1,24 +1,14 @@
 import dbus
 
-bus = dbus.SessionBus()
-player = bus.get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
-property_interface = dbus.Interface(player, dbus_interface='org.freedesktop.DBus.Properties')
-
-STRING_UNAVAILABLE = ''
 STRING_ERROR = ''
-
+STRING_UNAVAILABLE = 'Clementine not installed'
 
 class Py3status:
-    """
-    """
     cache_timeout = 5
     format = u'♫ {current}'
 
     def myclementine(self):
-        """
-        Get metadata of currently playing song
-        """
-        if not self.py3.check_commands(['/home/atrament/oss-projects/Clementine/bin/clementine']):
+        if not self.py3.check_commands(['clementine']):
             return {
                 'cached_until': self.py3.CACHE_FOREVER,
                 'color': self.py3.COLOR_BAD,
@@ -26,6 +16,10 @@ class Py3status:
             }
         
         try:
+            bus = dbus.SessionBus()
+            player = bus.get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
+            property_interface = dbus.Interface(player, dbus_interface='org.freedesktop.DBus.Properties')
+
             metadata = player.Get('org.mpris.MediaPlayer2.Player','Metadata', dbus_interface='org.freedesktop.DBus.Properties')
             artist = metadata['xesam:artist'][0] if 'xesam:artist' in metadata else ''
             title = metadata['xesam:title'] if 'xesam:title' in metadata else ''
@@ -44,5 +38,3 @@ class Py3status:
 if __name__ == "__main__":
      from py3status.module_test import module_test
      module_test(Py3status)
-
-
